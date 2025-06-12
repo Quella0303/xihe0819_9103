20b665f58083b586ddef6884ebbdfc420a6c3e02
Quella0303 <quella.he@yahoo.com>
2025-06-12 20:11:38 +1000

    Added basic Perlin-driven motion to dove visual

--- Quella/sketch_Quella.js
+++ Quella/sketch_Quella.js
@@ -2,7 +2,7 @@ let doveImg;
 let strokes = [];
 
 function preload() {
-  doveImg = loadImage("assets/dovefinal.png");
+  doveImg = loadImage("assets/dovefinal.png"); 
 }
 
 function setup() {
@@ -14,31 +14,63 @@ function setup() {
   let xOffset = (width - doveImg.width) / 2;
   let yOffset = (height - doveImg.height) / 2;
 
-  for (let y = 0; y < doveImg.height; y += 4) {
-    for (let x = 0; x < doveImg.width; x += 4) {
-      let index = (x + y * doveImg.width) * 4;
-      let r = doveImg.pixels[index];
-      let g = doveImg.pixels[index + 1];
-      let b = doveImg.pixels[index + 2];
-      let brightness = (r + g + b) / 3;
+  for (let layer = 0; layer < 3; layer++) {
+    for (let y = 0; y < doveImg.height; y += 4) {
+      for (let x = 0; x < doveImg.width; x += 4) {
+        let index = (x + y * doveImg.width) * 4;
+        let r = doveImg.pixels[index];
+        let g = doveImg.pixels[index + 1];
+        let b = doveImg.pixels[index + 2];
+        let brightness = (r + g + b) / 3;
 
-      if (brightness > 50) {
-        strokes.push({ x: x + xOffset, y: y + yOffset, col: color(r, g, b) });
+        
+        if (brightness > 50 && random() > 0.6) {
+          strokes.push(new BrushStroke(
+            x + xOffset,
+            y + yOffset,
+            color(r, g, b),
+            layer
+          ));
+        }
       }
     }
   }
-
-  noLoop(); 
 }
 
 function draw() {
   background(0);
-  noStroke();
-  for (let s of strokes) {
-    fill(s.col);
-    ellipse(s.x, s.y, 4, 4);
+  let t = millis() * 0.001;
+
+  for (let stroke of strokes) {
+    stroke.update(t);
+    stroke.display();
   }
 }
+
+class BrushStroke {
+  constructor(x, y, col, layer) {
+    this.origin = createVector(x, y);
+    this.pos = createVector(x, y);
+    this.color = col;
+    this.layer = layer;
+    this.seed = random(1000); 
+  }
+
+  update(time) {
+    let noiseVal = noise(this.origin.x * 0.005, this.origin.y * 0.005, time + this.seed);
+    let angle = noiseVal * TWO_PI * 2;
+    let radius = 2 + this.layer * 2;
+    this.pos.x = this.origin.x + cos(angle) * radius;
+    this.pos.y = this.origin.y + sin(angle) * radius;
+  }
+
+  display() {
+    noStroke();
+    fill(this.color);
+    ellipse(this.pos.x, this.pos.y, 4, 4);
+  }
+}
+
 
 
 
